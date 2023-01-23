ebae72a8ff6b1cf8bf2c19e819f30b70adaffe16
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-23 09:28:00 +0700

    Smart contract

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -9,65 +9,73 @@ import {DefaultOperatorFilterer} from "./DefaultOperatorFilterer.sol";
 contract NFT is Ownable, ERC721A, DefaultOperatorFilterer {
     string public uriPrefix = '';
     string public uriSuffix = '.json';
-    uint256 public max_supply = 10_000;
-    uint256 public burnAmount;
 
-    mapping(address => uint256[]) public scratched;
+    struct BoxData {
+        string name;
+        uint256 price;
+        uint256 burnAmount;
+        string cid;
+        uint256 maxSupply;
+        uint256 remainingSupply;
+        bool mintEnabled;
+    }
 
-    uint256 public amountMintPerAccount = 2;
-    bool public mintEnabled;
-    uint256 public price = 0 ether;
+    BoxData[] public boxes;
+    mapping (uint256 => uint256) idToBoxId;
 
     event MintSuccessful(address user);
 
-    constructor() ERC721A("Genesis Scratchy Card", "GSC") { }
+    constructor() ERC721A("Mystery Box", "MB") {
+        boxes.push(BoxData("Mystery Box 1", 0, 0, "QmYLpp6TaXjHPENgbDWRWzBQoJuc4zRE5z3sXjXhdYALp3", 100, 100, true));
+        boxes.push(BoxData("Mystery Box 2", 0, 0, "QmZnXGbXgpBNjAJaaaoWsH5VYoTNkVgpVmbpgFXnTUyQ5d", 100, 100, true));
+    }
 
-    function mint(uint256 quantity) external payable {
-        require(mintEnabled, 'Minting is not enabled');
-        require(totalSupply() + quantity < max_supply, 'Cannot mint more than max supply');
-        require(balanceOf(msg.sender) + quantity <= amountMintPerAccount, 'Each address may only mint x NFTs!');
-        require(msg.value >= getPrice() * quantity, "Not enough ETH sent; check price!");
+    function mint(uint256 _boxId, uint256 _quantity) external payable {
+        require(_boxId < boxes.length, "boxId out of range");
+        require(boxes[_boxId].mintEnabled, 'Minting is not enabled');
+        require(boxes[_boxId].remainingSupply > 0, 'Cannot mint more than max supply');
+        require(msg.value >= getPrice(_boxId) * _quantity, "Not enough ETH sent; check price!");
 
-        _mint(msg.sender, quantity);
+        for(uint256 i = 0; i < _quantity;) {
+            idToBoxId[_startTokenId() + _totalMinted() + i] = _boxId;
+            unchecked { ++i; }
+        }
+        _mint(msg.sender, _quantity);
+        boxes[_boxId].remainingSupply --;
 
         emit MintSuccessful(msg.sender);
     }
 
     function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
         require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
-
-        string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0
-            ? string(abi.encodePacked(currentBaseURI, Strings.toString(_tokenId), uriSuffix))
-            : '';
+        require(idToBoxId[_tokenId] < boxes.length, "boxId out of range");
+        return string(abi.encodePacked("ipfs://", boxes[idToBoxId[_tokenId]].cid, "/", Strings.toString(_tokenId), uriSuffix));
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmYLpp6TaXjHPENgbDWRWzBQoJuc4zRE5z3sXjXhdYALp3/";
-    }
+    // function _baseURI() internal pure override returns (string memory) {
+    //     return "QmYLpp6TaXjHPENgbDWRWzBQoJuc4zRE5z3sXjXhdYALp3";
+    // }
     
-    function baseTokenURI() public pure returns (string memory) {
-        return _baseURI();
-    }
+    // function baseTokenURI() public pure returns (string memory) {
+    //     return _baseURI();
+    // }
 
     function contractURI() public pure returns (string memory) {
         return "ipfs://QmZn7J1NDdFgJm5Q7pxmivZFKVDjcBcpU9sN1kezBUZpLn/";
     }
 
-    function setAmountMintPerAccount(uint _amountMintPerAccount) public onlyOwner {
-        amountMintPerAccount = _amountMintPerAccount;
+    function getPrice(uint256 _boxId) view public returns(uint) {
+        return boxes[_boxId].price;
     }
 
-    function getPrice() view public returns(uint) {
-        return price;
+    function setPrice(uint256 _boxId, uint _price) public onlyOwner {
+        require(_boxId < boxes.length, "boxId out of range");
+        boxes[_boxId].price = _price;
     }
 
-    function setPrice(uint _price) public onlyOwner {
-        price = _price;
-    }
-
-    function setMintEnabled(bool _state) public onlyOwner {
-        mintEnabled = _state;
+    function setMintEnabled(uint256 _boxId, bool _state) public onlyOwner {
+        require(_boxId < boxes.length, "boxId out of range");
+        boxes[_boxId].mintEnabled = _state;
     }
 
     function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
@@ -94,15 +102,25 @@ contract NFT is Ownable, ERC721A, DefaultOperatorFilterer {
         return 1;
     }
 
-    function scratch(uint256 _tokenId) public {
-        require(msg.sender == ownerOf(_tokenId), "You don't have the right to scratch this NFT");
-        _burn(_tokenId);
-        burnAmount += 1;
+    // Mystery Box functions
 
-        scratched[msg.sender].push(_tokenId);
+    function addMysteryBox(string memory _name, uint256 _price, string memory _cid, uint256 _maxSupply) public onlyOwner {
+        boxes.push(BoxData(_name, _price, 0, _cid, _maxSupply, _maxSupply, true));
     }
 
-    function getScratched(address _user) public view returns(uint256[] memory) {
-        return scratched[_user];
+    // function removeMysteryBox(uint256 _boxId) public onlyOwner {
+    //     require(_boxId < boxes.length, "boxId out of range");
+    //     boxes[_boxId] = boxes[boxes.length - 1];
+    //     boxes.pop();
+    // }
+
+    function getMysteryBoxName(uint256 _boxId) public view returns (string memory) {
+        require(_boxId < boxes.length, "boxId out of range");
+        return boxes[_boxId].name;
+    }
+
+    function getMysteryBoxCid(uint256 _boxId) public view returns (string memory) {
+        require(_boxId < boxes.length, "boxId out of range");
+        return boxes[_boxId].cid;
     }
 }
