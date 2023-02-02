9fd9cd742fd53e15a416106ea562cd3b26bba089
Alexis Davidson <alexisdavidson00@gmail.com>
2023-02-02 17:01:56 +0700

    Prepare for deploy

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -6,16 +6,51 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", fromWei(await deployer.getBalance()));
   
-  const NFT = await ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy();
-  console.log("NFT contract address", nft.address)
-  saveFrontendFiles(nft, "NFT");
+  const NftBox = await ethers.getContractFactory("NftBox");
+  const NftEgg = await ethers.getContractFactory("NftEgg");
+  const NftSneaker = await ethers.getContractFactory("NftSneaker");
+  const NftSneakerX = await ethers.getContractFactory("NftSneakerX");
+  const Equip = await ethers.getContractFactory("Equip");
+  const Usdc = await ethers.getContractFactory("Erc20Usdc");
+
+  // Deploy Usdc ERC20 Only for tests
+  usdc = await Usdc.deploy();
+  console.log("Usdc contract address", usdc.address)
+  saveFrontendFiles(usdc, "Usdc");
+  const usdcAddress = usdc.address
+
+  // Deploy contracts
+  nftEgg = await NftEgg.deploy();
+  console.log("NftEgg contract address", nftEgg.address)
+  saveFrontendFiles(nftEgg, "NftEgg");
+
+  nftSneaker = await NftSneaker.deploy();
+  console.log("NftSneaker contract address", nftSneaker.address)
+  saveFrontendFiles(nftSneaker, "NftSneaker");
+
+  nftSneakerX = await NftSneakerX.deploy();
+  console.log("NftSneakerX contract address", nftSneakerX.address)
+  saveFrontendFiles(nftSneakerX, "NftSneakerX");
+
+  nftBox = await NftBox.deploy(usdcAddress, nftSneaker.address, nftEgg.address);
+  console.log("NftBox contract address", nftBox.address)
+  saveFrontendFiles(nftBox, "NftBox");
+
+  equip = await Equip.deploy(nftSneakerX.address, nftSneaker.address, nftEgg.address);
+  console.log("Equip contract address", equip.address)
+  saveFrontendFiles(equip, "Equip");
+
+  await nftSneaker.setBoxAddress(nftBox.address);
+  await nftEgg.setBoxAddress(nftBox.address);
+  await nftSneaker.setEquipAddress(equip.address);
+  await nftEgg.setEquipAddress(equip.address);
+  await nftSneakerX.setEquipAddress(equip.address);
 
   // For testing
-  // await nft.setMintEnabled(true);
-  // // await nft.setPrice(0); // Price is already 0
-  // await nft.mint(1, { value: 0});
-  // console.log("Goerli test functions called")
+  await nftBox.addMysteryBox("Mystery Box 1", toWei(80), "QmSABpZp4i6HFoY4AcmKhPG5nujQXmVv8TosqNkvkY6t5n/1", 50)
+  await nftBox.addMysteryBox("Mystery Box 2", toWei(350), "QmSABpZp4i6HFoY4AcmKhPG5nujQXmVv8TosqNkvkY6t5n/2", 50)
+
+  console.log("Goerli test functions called")
 }
 
 function saveFrontendFiles(contract, name) {
